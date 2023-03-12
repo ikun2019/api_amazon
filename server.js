b36606478a45da579a3c2d5436038773441f7899
ikun <0215.miyatto@gmail.com>
2023-03-12 22:43:50 +0900

    データベースとの接続設定

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 const dotenv = require('dotenv');
 dotenv.config();
+const sequelize = require('./config/database');
 const express = require('express');
 
 const app = express();
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running PORT:${process.env.PORT}`)
-});
+// * appの設定
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+
+// * データベースと接続してサーバー起動
+sequelize
+  .sync()
+  // .sync({ alter: true })
+  .then(result => {
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is running PORT:${process.env.PORT}`)
+    });
+  })
+  .catch(err => {
+    console.log(err);
+  });
