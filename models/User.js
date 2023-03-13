b1ac2360e884fae9c19efa94639bc63b9cc90140
ikun <0215.miyatto@gmail.com>
2023-03-13 19:13:54 +0900

    Userモデルの作成

--- models/User.js
+++ models/User.js
@@ -2,7 +2,24 @@ const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
 
 const User = sequelize.define('user', {
-
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncremant: true,
+    allowNull: false,
+    primaryKey: true
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
 });
 
 module.exports = User;
