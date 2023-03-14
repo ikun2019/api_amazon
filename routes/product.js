2d4c39ad89ddfa922e7422ab1154217b99dbd7a5
ikun <0215.miyatto@gmail.com>
2023-03-15 05:27:58 +0900

    productのupdate機能の実装

--- routes/product.js
+++ routes/product.js
@@ -2,14 +2,20 @@ const router = require('express').Router();
 const productsController = require('../controllers/products');
 
 // ! Create a Product
-// => /api/products
+// * POST => /api/products
 router.post('/products', productsController.postAddProduct);
 
 // ! Get All Products
+// * GET => /api/products
+router.get('/products', productsController.getProducts);
 
 // ! Get a Single Product
+// * GET => /api/products/:id
+router.get('/products/:id', productsController.getProduct);
 
 // ! Update a Product
+// * PUT => /api/products/:id
+router.put('/products/:id', productsController.updateProduct);
 
 // ! Delete a single Product
 
